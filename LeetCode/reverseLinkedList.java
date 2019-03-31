a114fa901b775319f881100c7c9ffc1fac5d7b8e
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2019-03-31 13:44:27 -0700

    Leetcode #206: Reverse Singly Linked List

---
+++ LeetCode/reverseLinkedList.java
@@ -0,0 +1,36 @@
+/**
+ * File ReverseString.java
+ * @author Nam Hoang Nguyen
+ * @version 03/31/2019
+ *
+ * Reverse a singly linked list.
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {return null;}
+
+        /* iterative solution */
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
