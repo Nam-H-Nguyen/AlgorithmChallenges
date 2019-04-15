160e49ce214176cba0dcf3f15485549314d7f63a
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2019-04-14 23:05:51 -0700

    Finish contains duplicate algorithm using set and array data structure

---
+++ LeetCode/src/src/ContainDuplicate.java
@@ -0,0 +1,28 @@
+public class ContainsDuplicate {
+
+//    public boolean containsDuplicate(int[] nums) {
+//        Set<Integer> set = new HashSet<>(nums.length);
+//        for (int num : nums) {
+//            if (set.contains(num)) {
+//                return true;
+//            }
+//            set.add(num);
+//        }
+//        return false;
+//    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length-1; ++i) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
